185541a1402ec296a9f3034a1b7c582308b61272
Joseph Nipko <joe@nipko.com>
2024-09-18 11:08:04 -0700

    updated with new optimization logic

--- optimum.py
+++ optimum.py
@@ -2,25 +2,21 @@ from scipy.optimize import differential_evolution, NonlinearConstraint
 import numpy as np
 
 # Define the objective function
-def objective(x, df, min_trades):
+def objective(x, df, min_trades, min_trades_boost, win_rate_boost):
 
-    # Decision variables
-    VIX_overnight_min, VIX_overnight_max = x[0], x[1]
-    SPX_overnight_min, SPX_overnight_max = x[2], x[3]
-    VIX_prior_min, VIX_prior_max = x[4], x[5]
-
-    
     # Filter data based on the decision variables
     filtered_data = df[
-        (df['VIX_ON_CHANGE'] >= VIX_overnight_min) & (df['VIX_ON_CHANGE'] <= VIX_overnight_max) &
-        (df['SPX_ON_CHANGE'] >= SPX_overnight_min) & (df['SPX_ON_CHANGE'] <= SPX_overnight_max) &
-        (df['VIX_CURRENT_CLOSE'] >= VIX_prior_min) & (df['VIX_CURRENT_CLOSE'] <= VIX_prior_max)
+        (df['VIX_ON_CHANGE'] >= x[0]) & (df['VIX_ON_CHANGE'] <= x[1]) &
+        (df['SPX_ON_CHANGE'] >= x[2]) & (df['SPX_ON_CHANGE'] <= x[3]) &
+        (df['VIX_CURRENT_CLOSE'] >= x[4]) & (df['VIX_CURRENT_CLOSE'] <= x[5]) &
+        (df['SPX_CHANGE_FROM_OPEN'] >= x[6]) & (df['SPX_CHANGE_FROM_OPEN'] <= x[7]) 
+        #(df['VIX_CHANGE_FROM_OPEN'] >= x[8]) & (df['VIX_CHANGE_FROM_OPEN'] <= x[9]) 
     ]
     
     # Objectives
     num_trades = len(filtered_data)  # Maximize number of trades
-    total_profit = filtered_data['P/L'].sum() / 1000 # Maximize total profit
-    win_rate = 1000*( filtered_data['P/L'] > 0.0).mean() if num_trades > 0 else 0  # Maximize win rate
+    total_profit = ( filtered_data['P/L'] * filtered_data['time_wt']).sum()  # Maximize total profit
+    win_rate = 10000*( ( filtered_data['P/L'] > 0.0) * filtered_data['time_wt']).mean() if num_trades > 0 else 0  # Maximize win rate
 
     # Combined objective function (to minimize)
     # Include a penalty for too small ranges
@@ -29,43 +25,53 @@ def objective(x, df, min_trades):
 
     # Penalty for not meeting the minimum number of trades
     if num_trades < min_trades:
-        min_trades_penalty = 1000 * (min_trades - num_trades)  # Large penalty
+        min_trades_penalty = min_trades_boost * (min_trades - num_trades)  # Large penalty
     else:
         min_trades_penalty = 0  # No penalty if the constraint is met
     
     # Define weights for each objective
-    w2, w3 =  0.1, 0.8  # Adjust weights according to preference
+    # w2, w3 =  0.1, 0.8  # Adjust weights according to preference
     
     # Combined objective function (to minimize)
-    return -( w2 * total_profit + w3 * win_rate) + min_trades_penalty
+    return -( total_profit + win_rate_boost * win_rate) + min_trades_penalty
 
 
-def perform_optimization(target_dat):
+def perform_optimization(target_dat, min_trades,  min_trades_boost, win_rate_boost):
 
     bounds = [
-        (target_dat.VIX_ON_CHANGE.min(), target_dat.VIX_ON_CHANGE.max() - 0.01 ),  # VIX_overnight_min
-        (target_dat.VIX_ON_CHANGE.min() + 0.01, target_dat.VIX_ON_CHANGE.max()),  # VIX_overnight_max
-        (target_dat.SPX_ON_CHANGE.min(), target_dat.SPX_ON_CHANGE.max() -0.01),  # SPX_overnight_min
-        (target_dat.SPX_ON_CHANGE.min() + 0.01 , target_dat.SPX_ON_CHANGE.max()),  # SPX_overnight_max
-        (target_dat.VIX_CURRENT_CLOSE.min(), target_dat.VIX_CURRENT_CLOSE.max()-1),  # VIX_prior_min
-        (target_dat.VIX_CURRENT_CLOSE.min()+1, target_dat.VIX_CURRENT_CLOSE.max())  # VIX_prior_max
+        (target_dat.VIX_ON_CHANGE.min(), target_dat.VIX_ON_CHANGE.max() - 0.01 ), 
+        (target_dat.VIX_ON_CHANGE.min() + 0.01, target_dat.VIX_ON_CHANGE.max()), 
+        (target_dat.SPX_ON_CHANGE.min(), target_dat.SPX_ON_CHANGE.max() -0.01),  
+        (target_dat.SPX_ON_CHANGE.min() + 0.01 , target_dat.SPX_ON_CHANGE.max()),  
+
+
+        (target_dat.VIX_CURRENT_CLOSE.min(), target_dat.VIX_CURRENT_CLOSE.max()-1), 
+        (target_dat.VIX_CURRENT_CLOSE.min()+1, target_dat.VIX_CURRENT_CLOSE.max()),  
+
+        (target_dat.SPX_CHANGE_FROM_OPEN.min(), target_dat.SPX_CHANGE_FROM_OPEN.max() -0.01),  
+        (target_dat.SPX_CHANGE_FROM_OPEN.min() + 0.01 , target_dat.SPX_CHANGE_FROM_OPEN.max()) 
+
+        #(target_dat.VIX_CHANGE_FROM_OPEN.min(), target_dat.VIX_CHANGE_FROM_OPEN.max() -0.01), 
+        #(target_dat.VIX_CHANGE_FROM_OPEN.min() + 0.01 , target_dat.VIX_CHANGE_FROM_OPEN.max()),  
     ]
 
     # Define constraints to ensure min is less than max for each range using NonlinearConstraint
     constraints = [
         NonlinearConstraint(lambda x: x[1] - x[0], 0, np.inf),  # VIX_overnight_max > VIX_overnight_min
         NonlinearConstraint(lambda x: x[3] - x[2], 0, np.inf),  # SPX_overnight_max > SPX_overnight_min
-        NonlinearConstraint(lambda x: x[5] - x[4], 0, np.inf)   # VIX_prior_max > VIX_prior_min
+        NonlinearConstraint(lambda x: x[5] - x[4], 0, np.inf),   # VIX_prior_max > VIX_prior_min
+        NonlinearConstraint(lambda x: x[7] - x[6], 0, np.inf),   # SPX_change_max > SPX_change_min
+        #NonlinearConstraint(lambda x: x[9] - x[8], 0, np.inf),   # VIX_change_max > VIX_change_min
     ]
 
     # Define initial guess (midpoints of bounds)
-    x0 = [-0.05, 0.05, -0.01, 0.01, 15, 20]
+    x0 = [-0.05, 0.05, -0.01, 0.01, 15, 20, -0.01, 0.01] #, -0.01, 0.01]
 
     # Perform the optimization using differential evolution
     result = differential_evolution(
         objective,
         bounds,
-        args=(target_dat,50,),
+        args=(target_dat ,min_trades, min_trades_boost, win_rate_boost),
         strategy='best1bin',  # Strategy for differential evolution
         maxiter=1000,  # Maximum iterations
         popsize=17,  # Population size
@@ -88,6 +94,11 @@ def perform_optimization(target_dat):
         res_string = res_string + f"SPX GAP MAX: {np.round( result.x[3] * 100 ,2)}%\n"
         res_string = res_string + f"VIX MIN: {np.round( result.x[4],2)}\n"
         res_string = res_string + f"VIX MAX: {np.round( result.x[5],2)}\n"
+
+        res_string = res_string + f"SPX CHANGE MIN: {np.round( result.x[6]*100,2)}%\n"
+        res_string = res_string + f"SPX CHANGE MAX: {np.round( result.x[7]*100,2)}%\n"
+        # res_string = res_string + f"VIX CHANGE MIN: {np.round( result.x[8]*100,2)}%\n"
+        # res_string = res_string + f"VIX CHANGE MAX: {np.round( result.x[9]*100,2)}%\n"
 
         print("\n\n***********************************************")
         print(    "************   OPTIMIAL VALUES  ***************")
--- utils.py
+++ utils.py
@@ -1,6 +1,41 @@
 
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
+
+def calculate_time_weights(df, weight_range=(0.5, 1.0), method='linear'):
+    """
+    Calculate time-based weights for a DataFrame based on its index.
+    
+    Parameters:
+    df (pd.DataFrame): The DataFrame with a time-based index.
+    weight_range (tuple): The range of weights (min_weight, max_weight).
+    method (str): The type of weighting ('linear' or 'exponential').
+    
+    Returns:
+    pd.Series: A Series of weights.
+    """
+    
+    # Ensure the index is sorted by time
+    df = df.sort_index()
+    
+    # Calculate the time differences from the most recent observation
+    time_deltas = (df.index.max() - df.index).days  # Time delta in days
+
+    # Normalize the time differences to the range [0, 1]
+    normalized_time = time_deltas / time_deltas.max()
+
+    # Apply the chosen weighting method
+    if method == 'linear':
+        # Linear interpolation between weight_range
+        weights = weight_range[0] + (weight_range[1] - weight_range[0]) * (1 - normalized_time)
+    elif method == 'exponential':
+        # Exponential decay based on normalized time
+        weights = weight_range[0] + (weight_range[1] - weight_range[0]) * np.exp(-normalized_time)
+    else:
+        raise ValueError("Invalid method. Choose 'linear' or 'exponential'.")
+    
+    return pd.Series(weights, index=df.index)
 
 
 def plot_candlesticks( df, tar_date, ticker):
@@ -46,7 +81,7 @@ def calc_percent_change_from_open(df, colname):
     return df[[target_col, open_col, close_col]]
 
 
-def gen_data(input_file):
+def gen_data(input_file, time_weight):
 
     blackout_dts = '2022-05-17,2022-05-24,2022-06-17,2022-06-29,2022-08-26,2022-09-08,2022-09-23,2022-09-27,2022-09-28,2022-11-30,2023-01-10,2023-02-07,2023-05-19,2023-06-28,2023-06-29,2023-08-25,2023-09-28,2023-10-02,2023-10-19,2023-10-25,2023-11-08,2023-11-09,2023-12-01,2024-02-05,2024-03-22,2024-03-29,2024-04-03,2024-04-16,2024-05-14,2024-05-19,2022-06-10,2022-07-13,2022-08-10,2022-09-13,2022-10-13,2022-11-10,2022-12-13,2023-01-12,2023-02-14,2023-03-14,2023-04-12,2023-05-10,2023-06-13,2023-07-12,2023-08-10,2023-09-13,2023-10-12,2023-11-14,2023-12-12,2024-01-11,2024-02-13,2024-03-12,2024-04-10,2024-05-15,2024-06-12,2024-07-11,2024-08-14,2024-09-11,2024-10-10,2024-11-13,2024-12-11,2022-05-25,2022-07-06,2022-08-17,2022-10-12,2022-11-23,2023-01-04,2023-02-22,2023-04-12,2023-05-24,2023-07-05,2023-08-16,2023-10-11,2023-11-21,2024-01-03,2024-02-21,2024-04-10,2024-05-22,2024-07-03,2024-08-21,2024-10-09,2024-11-27,2022-06-15,2022-07-27,2022-09-21,2022-11-02,2022-12-14,2023-02-01,2023-03-22,2023-05-03,2023-06-14,2023-07-26,2023-09-20,2023-11-01,2023-12-13,2024-01-31,2024-03-20,2024-05-01,2024-06-12,2024-07-31,2024-09-18,2024-11-06,2024-12-18'
     blackout_dts = blackout_dts.split(',')
@@ -66,6 +101,13 @@ def gen_data(input_file):
     ).set_index('OpenDatetime')
 
     target_dat.index = target_dat.index - pd.DateOffset(hours=1)
+
+
+    # calculate the time weighting
+
+    target_dat['time_wt'] = calculate_time_weights(target_dat, 
+                                                   weight_range=time_weight['range'], 
+                                                   method=time_weight['method'])
 
 
     # Read in the VIX and SPX per minute data
@@ -91,6 +133,9 @@ def gen_data(input_file):
     # Filter data to include only market open to close times
     VIX_DATA = VIX_DATA.between_time(vix_market_open_time, market_close_time)
     SPX_DATA = SPX_DATA.between_time(spx_market_open_time, market_close_time)
+
+
+
 
 
     VIX_OVERNIGHT_CHANGE = calc_open_close_change(VIX_DATA, 'VIX')
